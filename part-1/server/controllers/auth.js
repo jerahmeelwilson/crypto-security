b462a757150e7cab75e6027ff97190b38d500c90
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-04 15:53:12 -0500

    Added solutions for part 1

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,21 +1,37 @@
-const users = []
+const bcrypt = require('bcryptjs');
+const e = require('cors');
+
+const users = [];
 
 module.exports = {
     login: (req, res) => {
       console.log('Logging In User')
       console.log(req.body)
       const { username, password } = req.body
+      let userfound = false;
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
-        }
+        if (users[i].username === username){
+          userfound = true;
+          bcrypt.compare(password,users[i].password).then(result => {
+            if(result){
+              console.log(result);
+              res.status(200).send(users[i]);
+            }
+        }).catch(err => console.log(err));
       }
-      res.status(400).send("User not found.")
-    },
+      if(userfound == false){
+        res.status(400).send("User not found.");
+      }
+    }
+  },
     register: (req, res) => {
         console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
+        let salt = bcrypt.genSaltSync(10);
+        let hash = bcrypt.hashSync(req.body.password,salt);
+        let user = req.body;
+        user.password = hash;
+        users.push(user);
+        console.log(users);
+        res.status(200).send(user);
     }
 }
